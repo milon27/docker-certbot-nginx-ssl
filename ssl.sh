02311028c510637e349954cfc83333c24b21da90
milon27 <mdjahidulislammilon@gmail.com>
2022-04-02 23:28:18 +0600

    few done

--- ssl.sh
+++ ssl.sh
@@ -1,9 +1,14 @@
 git checkout nossl
 docker compose up nginx -d
 read -p "If site is live, Press enter to continue"
+
+# until $(curl --output /dev/null --silent --head --fail http://testmess27.ml/); do
+#   printf 'waiting...'
+#   sleep 5
+# done
+
 git checkout master
 docker compose up certbot
 
 echo 'all done, restart nginx'
-
-# docker compose up nginx -d
+docker compose up nginx -d

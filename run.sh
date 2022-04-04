caa109d31063f9ba9a4c4fd0b625ab58fc8946f1
milon27 <mdjahidulislammilon@gmail.com>
2022-04-04 16:07:48 +0600

    ok

--- run.sh
+++ run.sh
@@ -3,17 +3,15 @@ docker compose -f docker-compose.dev.yml up -d
 # read -p "If site is live, Press enter to continue"
 # curl -sL -w "%{http_code}\n" "http://m27lab.ml/" -o /dev/null output is 200
 
-sleep 7
-
 VARI=$(curl -sL -w "%{http_code}\n" "http://m27lab.ml/" -o /dev/null)
-
 echo "response = $VARI"
 
-# until [ $VARI == "200" ]
-# do
-#   sleep 5
-#   echo "response = $VARI"
-# done
+until [ $VARI == "200" ]
+do
+  sleep 3
+  VARI=$(curl -sL -w "%{http_code}\n" "http://m27lab.ml/" -o /dev/null)
+  echo "3 second later res = $VARI"
+done
 
 echo "loop done,go next"
 docker compose up certbot
